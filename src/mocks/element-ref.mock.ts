ff78691de324bdf002538efe167df168383e6974
yufilatov <jurgenfilatov@gmail.com>
2021-01-13 19:20:12 +0300

    Auto-generated commit

--- src/mocks/element-ref.mock.ts
+++
@@ -1,6 +0,0 @@
-import { ElementRef } from '@angular/core';
-import { MockNativeElement } from './native-element.mock';
-
-export class MockElementRef extends ElementRef {
-    constructor() { super(new MockNativeElement()); }
-}
--- src/mocks/renderer2.mock.ts
+++
@@ -1,6 +0,0 @@
-
-// export const createSpyRenderer2 = () =>
-//     jasmine.createSpyObj('MockRenderer2', [
-//         'createElement',
-//         'appendChild',
-//     ]);
