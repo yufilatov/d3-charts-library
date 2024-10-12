301522870f237275f8ab3735ef5ec2879032fb7d
Yury Filatov <jurgengottlieb@gmail.com>
2024-10-12 19:00:36 +0500

    Updates

--- src/components/chart-popup/chart-popup.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ChartPortalModule } from '../chart-portal/chart-portal.module';
-
-@NgModule({
-    imports: [ChartPortalModule],
-})
-export class ChartPopupModule { }
--- src/components/common/chart-scale.ts
+++
@@ -1,7 +0,0 @@
-export interface IChartScale {
-    (d: number): number;
-    domain: any;
-    range: any;
-}
-
-export type ChartScaleType = 'band' | 'ordinal' | 'linear' | 'linear-vertical' | 'band-vertical';
